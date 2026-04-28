24cd59cb49febb06f99abbe2566699ed4aef3a19
sjak1631 <akira.shoji.2004@gmail.com>
2026-04-28 10:43:47 +0900

    chore: remove TD3 import and related entry from __all__ in __init__.py

--- recursive_stable_baselines3/__init__.py
+++ recursive_stable_baselines3/__init__.py
@@ -1,7 +1,6 @@
 import os
 
 from recursive_stable_baselines3.recursive_ppo import Recursive_PPO, Recursive_PPO_multi_output
-from recursive_stable_baselines3.recursive_td3 import TD3
 
 # Read version from file
 version_file = os.path.join(os.path.dirname(__file__), "version.txt")
@@ -12,5 +11,4 @@ with open(version_file) as file_handler:
 __all__ = [
     "Recursive_PPO",
     "Recursive_PPO_multi_output",
-    "TD3",
 ]
