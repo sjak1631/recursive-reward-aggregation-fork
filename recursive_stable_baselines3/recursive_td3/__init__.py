b05dd13b4161a65d9de8dba838dd8d91715c10f2
sjak1631 <akira.shoji.2004@gmail.com>
2026-04-28 10:43:23 +0900

    chore: remove recursive TD3 implementation and associated policies

--- recursive_stable_baselines3/recursive_td3/__init__.py
+++
@@ -1,4 +0,0 @@
-from recursive_stable_baselines3.recursive_td3.policies import CnnPolicy, MlpPolicy, MultiInputPolicy
-from recursive_stable_baselines3.recursive_td3.td3 import TD3
-
-__all__ = ["TD3", "CnnPolicy", "MlpPolicy", "MultiInputPolicy"]
